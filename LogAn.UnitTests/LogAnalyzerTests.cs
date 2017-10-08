3d89e69e5e049bf184005b7e80592f142e5d7abe
Reist <malachov.evgeny@gmail.com>
2017-10-08 23:06:36 +0300

    Art of Unit Testing 2.4.2

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn.UnitTests
+{
+    public class LogAnalyzerTests
+    {
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            
+        }
+    }
+}
